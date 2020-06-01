5bab089f2830a720413d7c818a489ea998a23c38
Mantas Būdvytas <m.budvytas@gmail.com>
2020-06-01 12:36:52 +0300

    Added city delete

--- academytask/src/pages/Countries.js
+++ academytask/src/pages/Countries.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import addButton from '../images/addButton.png';
 import searchButton from '../images/searchButton.png';
+import trashButton from '../images/trash.png';
+import editButton from '../images/edit.png';
+//https://akademija.teltonika.lt/api1/
 
-function Header() {
+function Countries() {
+    const [countries, setCountries] = useState([]);
+    const [needsUpdate, setNeedsUpdate] = useState(true);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios.get(
+            `https://akademija.teltonika.lt/api1/countries`,
+          );
+
+          setCountries([...result.data.countires]);
+        };
+     
+        fetchData();
+        setNeedsUpdate(false);
+      }, [needsUpdate]);
+
+
+
 
     const handleRedirect = () => {
+        
+    }
+    const handleDelete = (id) => {
+        const fetchData = async () => {
+            const result = await axios.delete(
+            `https://akademija.teltonika.lt/api1/countries/${id}`,
+            );
+  
+            console.log(result.data.message);
+        };
+       
+        fetchData();
+    }
+    const handleEdit = () => {
         
     }
 
@@ -22,7 +59,7 @@ function Header() {
 
             <div className="Page__SearchNav flex-row">
                 <div className="Page__SearchNav__inputWrapper flex-row rectangle">
-                    <input className="Page__SearchNav__inputWrapper__input" type="text" placeholder="Search.."/>
+                    <input className="Page__SearchNav__inputWrapper__input" type="text"/>
                     <a className="Page__SearchNav__inputWrapper__search" onClick={ () => { handleRedirect('/') } }>
                         <img src={searchButton} alt="search Button" className="search-button"/>
                     </a>
@@ -56,32 +93,36 @@ function Header() {
                         veiksmai
                     </div>
                 </div>
-                <div className="Page__CountriesContainer__row flex-row">
-                    <p className="Page__CountriesContainer__row__text first">
-                        pavadinimas
-                    </p>
-                    <p className="Page__CountriesContainer__row__text">
-                        UŽIMAMAS PLOTAS
-                    </p>
-                    <p className="Page__CountriesContainer__row__text">
-                        GYVENTOJŲ SKAIČIUS
-                    </p>
-                    <p className="Page__CountriesContainer__row__text">
-                        šALIES TEL. KODAS
-                    </p>
-                    <p className="Page__CountriesContainer__row__options last flex-row">
-                        <a className="Page__CountriesContainer__row__options__item right-border" onClick={ () => { handleRedirect('/') } }>
-                            <img src={searchButton} alt="search Button" className="search-button"/>
-                        </a>
-                        <a className="Page__CountriesContainer__row__options__item" onClick={ () => { handleRedirect('/') } }>
-                            <img src={searchButton} alt="search Button" className="search-button"/>
-                        </a>     
-                    </p>
-                </div>
+                {
+                    countries.map((country) => {
+                        return (
+                            <div className="Page__CountriesContainer__row flex-row" key={country.id}>
+                                <p className="Page__CountriesContainer__row__text first">
+                                    {country.name}
+                                </p>
+                                <p className="Page__CountriesContainer__row__text">
+                                    {country.area}
+                                </p>
+                                <p className="Page__CountriesContainer__row__text">
+                                    {country.population}
+                                </p>
+                                <p className="Page__CountriesContainer__row__text">
+                                    {country.calling_code}
+                                </p>
+                                <p className="Page__CountriesContainer__row__options last flex-row">
+                                    <a className="Page__CountriesContainer__row__options__item" onClick={ () => { handleDelete(country.id) } }>
+                                        <img src={trashButton} alt="trash Button"/>
+                                    </a>
+                                    <a className="Page__CountriesContainer__row__options__item" onClick={ () => { handleEdit(country.id) } }>
+                                        <img src={editButton} alt="edit Button"/>
+                                    </a>     
+                                </p>
+                            </div>
+                        );
+                    })
+                }
             </div>
-
         </div>
         );
     }
-
-export default Header;
+export default Countries;
